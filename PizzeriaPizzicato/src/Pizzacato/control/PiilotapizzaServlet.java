58fe89e55bf30cd6d4402c8dd0b06f93e46e758c
a1500924 <a1500924@PAO5003Z13.haagahelia.amk>
2016-03-04 15:15:09 +0200

    Tein Piilotapizza-servletin

---
+++ PizzeriaPizzicato/src/Pizzacato/control/PiilotapizzaServlet.java
@@ -0,0 +1,41 @@
+package Pizzacato.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Pizzacato.model.dao.PizzaDAO;
+
+/**
+ * Marianne K.
+ */
+@WebServlet("/PiilotapizzaServlet")
+public class PiilotapizzaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		try{
+		
+		String piilotettavan_id = request.getParameter("id");
+		
+		PizzaDAO pizzadao = new PizzaDAO();
+		pizzadao.piilotaListalla(piilotettavan_id);
+		
+	} catch (SQLException e) {
+		
+		System.out.println("Sovelluksessa tapahtui virhe "+ e.getMessage());
+				
+	response.sendRedirect("Menu");	
+	}
+	
+
+}
+}

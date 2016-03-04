48c55bb78c8892305e96556a36ab6808ffaa5691
a1500941 <a1500941@PAO5003Z12.haagahelia.amk>
2016-03-04 12:19:21 +0200

    Lisäsin Pizza luokalle getterin listalla attribuutille ja lisäsin lisaaPizza metodin pizzaDAO:lle.

--- PizzeriaPizzicato/src/Pizzacato/model/dao/PizzaDAO.java
+++ PizzeriaPizzicato/src/Pizzacato/model/dao/PizzaDAO.java
@@ -1,6 +1,7 @@
 package Pizzacato.model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,11 +13,12 @@ public class PizzaDAO extends DataAccessObject{
 
 	
 	public ArrayList<Pizza> haePizzat() throws SQLException{
+		//HAETAAN KAIKKI PIZZAT
 		
 		// YHTEYS
 		Connection conn = getConnection();
 		
-		// LISTA PIZZOILLE
+		// TYHJÄ LISTA PIZZOILLE
 		ArrayList<Pizza> pizzat = new ArrayList<>();
 		
 		// HAKU LAUSE
@@ -24,6 +26,7 @@ public class PizzaDAO extends DataAccessObject{
 		Statement statement = conn.createStatement();
 		ResultSet results = statement.executeQuery(query);
 		
+		// ITEROI TULOKSET LÄPI => LUO UUSI PIZZA OLIO => LISÄÄ LISTAAN...
 		while(results.next()){
 			String id = results.getString(1);
 			String nimi = results.getString(2);
@@ -41,6 +44,25 @@ public class PizzaDAO extends DataAccessObject{
 	
 
 	public void lisaaPizza(Pizza pizza) throws SQLException{
+		// LISÄÄ UUSI PIZZA TIETOKANTAAN.
+		
+		// YHTEYS
+		Connection conn = getConnection();
+		
+		// LISÄYS LAUSE
+		String query = "INSERT INTO pizza(pizzaId, nimi, tayteId, kuvaus, listalla, hinta) VALUES(?, ?, ?, ?, ?, ?)";
+		PreparedStatement statement = conn.prepareStatement(query);
+		statement.setString(1, pizza.getPizzaId());
+		statement.setString(2, pizza.getNimi());
+		statement.setString(3, pizza.getTayteId());
+		statement.setString(4, pizza.getKuvaus());
+		statement.setBoolean(5, pizza.getListalla());
+		statement.setDouble(6, pizza.getHinta());
+		
+		int syotettiin = statement.executeUpdate();
+		if(syotettiin > 1){
+			System.out.println("uusi pizza lisättiin!");
+		}
 		
 	}
 	

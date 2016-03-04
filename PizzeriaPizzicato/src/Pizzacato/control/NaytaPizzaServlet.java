97f1c4bad3e4bf70b2918ea7a2319569fe9c8649
a1500846 <a1500846@PAO5003Z14.haagahelia.amk>
2016-03-04 15:24:25 +0200

    NaytaPizzaServlet

--- PizzeriaPizzicato/src/Pizzacato/control/NaytaPizzaServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/NaytaPizzaServlet.java
@@ -2,49 +2,48 @@ package Pizzacato.control;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Pizzacato.model.Pizza;
 import Pizzacato.model.dao.PizzaDAO;
 
 /**
  * Matias K
  */
-@WebServlet("/Menu")
-public class ListaaPizzatServlet extends HttpServlet {
+@WebServlet("/NaytaPizzaServlet")
+public class NaytaPizzaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+ 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		// TODO Auto-generated method stub
 		
-		// Luodaan pizzadao ja haetaan kaikki pizzat
-		PizzaDAO pizzadao = new PizzaDAO();
-		ArrayList<Pizza> pizzat = new ArrayList<>();
-		try {
-			pizzat = pizzadao.haePizzat();
-		} catch (SQLException e) {
-			System.out.println("Sovelluksessa tapahtui virhe "+ e.getMessage());
-			e.printStackTrace();
-		}
-		// Tallennetaan request-olion alle kaikki pizzat
-		request.setAttribute("pizzat", pizzat);
 		
-		String jsp = "/view/Menu.jsp";
-		RequestDispatcher dispather = getServletContext().getRequestDispatcher(jsp);
-		dispather.forward(request, response);
+		
 		
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
+		String naytettava_id = request.getParameter("id");
+		
+		PizzaDAO pizzadao = new PizzaDAO();
+		try {
+			pizzadao.naytaListalla(naytettava_id);
+		} catch (SQLException e) {
+			System.out.println("Sovelluksessa tapahtui virhe "+ e.getMessage());
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("Menu");
+		
 	}
 
 }

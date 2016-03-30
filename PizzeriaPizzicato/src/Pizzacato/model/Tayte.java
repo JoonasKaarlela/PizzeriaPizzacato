2c059c9725942270e7326b654fb1308ef5ed6f80
a1500846 <a1500846@PAO5003Z13.haagahelia.amk>
2016-03-30 13:53:40 +0300

    Tayte class

--- PizzeriaPizzicato/src/Pizzacato/model/Tayte.java
+++ PizzeriaPizzicato/src/Pizzacato/model/Tayte.java
@@ -3,18 +3,28 @@ package Pizzacato.model;
 public class Tayte {
 		
 		private String tayte_id;
-		private String ravintosisalto;
+		private String nimi;
+		private String alkupera;
+		private String kuvaus;
+		private double hinta;
 		
 
-public Tayte(String tayte_id, String ravintosisalto) {
+public Tayte(String tayte_id, String nimi, String alkupera, String kuvaus, Double hinta) {
 	super();
 	this.tayte_id = tayte_id;
-	this.ravintosisalto = ravintosisalto;
+	this.nimi = nimi;
+	this.alkupera = alkupera;
+	this.kuvaus = kuvaus;
+	this.hinta = hinta;
+	
 }
 
 public Tayte(){
 	tayte_id = "";
-	ravintosisalto ="";
+	nimi = "";
+	alkupera = "";
+	kuvaus = "";
+	hinta = 0.00;
 }
 
 public String getTayte_id() {
@@ -25,12 +35,35 @@ public void setTayte_id(String tayte_id) {
 	this.tayte_id = tayte_id;
 }
 
-public String getRavintosisalto() {
-	return ravintosisalto;
+public String getNimi() {
+	return nimi;
 }
 
-public void setRavintosisalto(String ravintosisalto) {
-	this.ravintosisalto = ravintosisalto;
+public void setNimi(String nimi){
+	this.nimi = nimi;
+}
+
+public String getAlkupera() {
+	return alkupera;
+}
+
+public void setAlkupera(String alkupera){
+	this.alkupera = alkupera;
+}
+
+public String getKuvaus () {
+	return kuvaus;
+}
+
+public void setKuvaus(String kuvaus){
+	this.kuvaus = kuvaus;
+}
+
+public double getHinta() {
+	return hinta;
+}
+public void setHinta(double hinta){
+	this.hinta = hinta;
 }
 
 @Override

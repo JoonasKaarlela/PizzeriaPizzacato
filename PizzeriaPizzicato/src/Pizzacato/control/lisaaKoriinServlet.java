79855966188ca907575e69889aced2cdf62f3df8
a1500941 <a1500941@PAO5003Z12.haagahelia.amk>
2016-03-30 15:16:00 +0300

    lisääKoriinServlet

---
+++ PizzeriaPizzicato/src/Pizzacato/control/lisaaKoriinServlet.java
@@ -0,0 +1,47 @@
+package Pizzacato.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Pizzacato.model.Pizza;
+import Pizzacato.model.dao.PizzaDAO;
+
+@WebServlet("/lisaaKoriin")
+public class lisaaKoriinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pizza_id = request.getParameter("id");
+		Pizza pizza = new Pizza();
+		
+		PizzaDAO pizzadao = new PizzaDAO();
+		try{
+			pizza = pizzadao.haePizza(pizza_id);
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		
+		if(pizza != null){
+			if(request.getSession(false).getAttribute("ostoskori") == null){
+				System.out.println("Tehdään uusi ostoskori...");
+				ArrayList<Pizza> ostoskori = new ArrayList<>();
+				ostoskori.add(pizza);
+				request.getSession(false).setAttribute("ostoskori", ostoskori);
+			} else {
+				ArrayList<Pizza> ostoskori = (ArrayList<Pizza>) request.getSession(false).getAttribute("ostoskori");
+				ostoskori.add(pizza);
+				request.getSession(false).setAttribute("ostoskori", ostoskori);
+			}
+		} else {
+			System.out.println("Pizzaa ei löytynyt");
+		}
+		
+	}
+}

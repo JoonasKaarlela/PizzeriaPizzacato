5cea467c59fb9af49b51b776720f17d33a24993f
a1500941 <a1500941@PAO5003Z12.haagahelia.amk>
2016-03-30 15:23:49 +0300

    Ostoskori servletit *fix*

--- PizzeriaPizzicato/src/Pizzacato/control/SuoritaTilausServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/SuoritaTilausServlet.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/suoritaTilausServlet")
-public class suoritaTilausServlet extends HttpServlet {
+@WebServlet("/suoritaTilaus")
+public class SuoritaTilausServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 

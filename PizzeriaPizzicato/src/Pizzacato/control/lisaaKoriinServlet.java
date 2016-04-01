e4407cac9f16c26d15b51eadab1eca2d1b497385
a1500941 <a1500941@PAO5003Z10.haagahelia.amk>
2016-04-01 13:01:23 +0300

    lisaaKoriinServlet refactorointi ja siivous

--- PizzeriaPizzicato/src/Pizzacato/control/lisaaKoriinServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/lisaaKoriinServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import Pizzacato.model.Pizza;
 import Pizzacato.model.dao.PizzaDAO;
@@ -18,31 +19,41 @@ public class lisaaKoriinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String pizza_id = request.getParameter("id");
-		Pizza pizza = new Pizza();
 		
+		String id = request.getParameter("id");
+		Pizza pizza = haePizza(id);
+		lisaaKoriin(pizza, request.getSession());
+		
+		response.sendRedirect("Menu");
+	}
+		
+	
+	public Pizza haePizza(String id){
+		Pizza pizza = null;
 		PizzaDAO pizzadao = new PizzaDAO();
 		try{
-			pizza = pizzadao.haePizza(pizza_id);
+			pizza = pizzadao.haePizza(id);
+			return pizza;
 		}catch(SQLException e){
 			System.out.println(e.getMessage());
+			return null;
 		}
-		
+	}
+	
+	
+	public void lisaaKoriin(Pizza pizza, HttpSession session){
 		if(pizza != null){
-			if(request.getSession(false).getAttribute("ostoskori") == null){
-				System.out.println("Tehdään uusi ostoskori...");
+			if(session.getAttribute("ostoskori") == null){
 				ArrayList<Pizza> ostoskori = new ArrayList<>();
 				ostoskori.add(pizza);
-				request.getSession(false).setAttribute("ostoskori", ostoskori);
+				session.setAttribute("ostoskori", ostoskori);
 			} else {
 				@SuppressWarnings("unchecked")
-				ArrayList<Pizza> ostoskori = (ArrayList<Pizza>) request.getSession(false).getAttribute("ostoskori");
+				ArrayList<Pizza> ostoskori = (ArrayList<Pizza>) session.getAttribute("ostoskori");
 				ostoskori.add(pizza);
-				request.getSession(false).setAttribute("ostoskori", ostoskori);
+				session.setAttribute("ostoskori", ostoskori);
 			}
-		} else {
-			System.out.println("Pizzaa ei löytynyt");
 		}
-		
 	}
+	
 }

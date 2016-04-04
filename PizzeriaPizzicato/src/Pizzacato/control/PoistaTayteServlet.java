0c9b30a63bf16bb30c78737da6f3da936094c146
a1500924 <a1500924@PAO5003Z13.haagahelia.amk>
2016-04-04 14:10:14 +0300

    Poistatäyteservlettii

--- PizzeriaPizzicato/src/Pizzacato/control/PoistaTayteServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/PoistaTayteServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import Pizzacato.model.dao.TayteDAO;
 
-@WebServlet("/PoistaTayteServlet")
+@WebServlet("/PoistaTayte")
 public class PoistaTayteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 

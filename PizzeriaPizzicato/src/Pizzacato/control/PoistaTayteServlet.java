0e5c6f74dfb257b128d4fc0df06a652205a863ab
a1500924 <a1500924@PAO5003Z13.haagahelia.amk>
2016-04-04 14:05:37 +0300

    PoistatäyteServlet

---
+++ PizzeriaPizzicato/src/Pizzacato/control/PoistaTayteServlet.java
@@ -0,0 +1,35 @@
+package Pizzacato.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Pizzacato.model.dao.TayteDAO;
+
+@WebServlet("/PoistaTayteServlet")
+public class PoistaTayteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		poistaTayte(id);
+
+		response.sendRedirect("taytteet");
+	}
+
+	public void poistaTayte(String id) {
+		TayteDAO taytedao = new TayteDAO();
+		try {
+			taytedao.poistaTayte(id);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}

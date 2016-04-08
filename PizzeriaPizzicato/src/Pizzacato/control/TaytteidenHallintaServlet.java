99c0e01dc65bb97a41b8e5790effc48aa808126b
a1500846 <a1500846@PAO5003Z14.haagahelia.amk>
2016-04-08 15:22:49 +0300

    fixed

--- PizzeriaPizzicato/src/Pizzacato/control/TaytteidenHallintaServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/TaytteidenHallintaServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/taytteidenhallinta")
+@WebServlet("/Taytteet")
 public class TaytteidenHallintaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        

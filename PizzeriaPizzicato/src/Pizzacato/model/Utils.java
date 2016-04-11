04ffe3032f2852e31a816c42a71d785f6b012632
Roope <Roope@DESKTOP-7HIBSRG>
2016-04-11 08:18:51 +0300

    Util metodit objectin kääntämiseen String luokan olioksi ja takaisin. ( Karsitaa database hakuja... )

--- PizzeriaPizzicato/src/Pizzacato/model/Utils.java
+++ PizzeriaPizzicato/src/Pizzacato/model/Utils.java
@@ -1,5 +1,13 @@
 package Pizzacato.model;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Random;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 public class Utils {
 	public String generate(int length){
@@ -8,5 +16,20 @@ public class Utils {
 			code += "" + new Random().nextInt(9);
 		}
 		return code;
+	}
+	public String objectToString(Object obj) throws IOException{
+		ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream();
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(bytearrayoutputstream);
+		objectOutputStream.writeObject(obj);
+		objectOutputStream.close();
+		String encoded = new String(Base64.encode(bytearrayoutputstream.toByteArray()));
+		return encoded;
+	}
+	
+	public Object stringToObject(String encoded) throws IOException, ClassNotFoundException{
+		byte[] bytes = Base64.decode(encoded);
+	    ObjectInputStream objectinputstream = new ObjectInputStream( new ByteArrayInputStream(bytes) );
+	    Object decoded = (Object) objectinputstream.readObject(); 
+		return decoded;
 	}
 }

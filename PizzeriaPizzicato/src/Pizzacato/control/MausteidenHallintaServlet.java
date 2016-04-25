60c1367b9cbbdccc25f5afb23e3e42759cc1c80d
a1500941 <a1500941@PAO5003Z12.haagahelia.amk>
2016-04-25 12:37:25 +0300

    404 selvisi

--- PizzeriaPizzicato/src/Pizzacato/control/MausteidenHallintaServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/MausteidenHallintaServlet.java
@@ -4,6 +4,7 @@ import Pizzacato.model.Mauste;
 import Pizzacato.model.dao.MausteDAO;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -18,9 +19,16 @@ public class MausteidenHallintaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dp = request.getServletContext().getRequestDispatcher("/view/Mausteet.jsp");´
+		RequestDispatcher dp = request.getServletContext().getRequestDispatcher("/view/Mausteet.jsp");
 		
-		ArrayList<Mauste> mausteet = new MausteDAO().haeMausteet();
+		ArrayList<Mauste> mausteet = new ArrayList<>();
+		
+		try{
+			mausteet = new MausteDAO().haeMausteet();
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		
 		request.setAttribute("mausteet", mausteet);
 		
 		dp.forward(request, response);

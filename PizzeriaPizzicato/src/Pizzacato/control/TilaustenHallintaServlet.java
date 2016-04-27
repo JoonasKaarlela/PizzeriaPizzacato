2430960e72963f2b4f311b43161df6e67883489c
a1500846 <a1500846@PAO5003Z14.haagahelia.amk>
2016-04-27 12:55:46 +0300

    TilaustenHallintaServlet

--- PizzeriaPizzicato/src/Pizzacato/control/TilaustenHallintaServlet.java
+++ PizzeriaPizzicato/src/Pizzacato/control/TilaustenHallintaServlet.java
@@ -1,11 +1,18 @@
 package Pizzacato.control;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Pizzacato.model.Tilaus;
+import Pizzacato.model.dao.TilausDAO;
 
 @WebServlet("/TilaustenHallinta")
 public class TilaustenHallintaServlet extends HttpServlet {
@@ -13,7 +20,26 @@ public class TilaustenHallintaServlet extends HttpServlet {
        
  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sivu = "/view/Admintilaukset.jsp";
 		
+		ArrayList<Tilaus> tilaukset = haeTilaukset();
+		request.setAttribute("tilaukset", tilaukset);
+		
+			
+		RequestDispatcher dp  = getServletContext().getRequestDispatcher(sivu);
+		dp.forward(request, response);
+		
+	}
+		
+		public ArrayList<Tilaus> haeTilaukset() {
+			ArrayList<Tilaus> tilaukset = new ArrayList<>();
+			TilausDAO tilausdao = new TilausDAO ();
+			try {
+				tilaukset = tilausdao.haeTilaukset(null);
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		return tilaukset;
 	}
 
 
